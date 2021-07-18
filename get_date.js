66308f072bcffcadb67b66185d5cac6bec4d16ea
E. Scott Menter <scott.menter@gmail.com>
2021-07-17 18:05:05 -0700

    still debugging json

--- get_date.js
+++ get_date.js
@@ -12,8 +12,8 @@ const yy = gregDate.getFullYear().toString().padStart(2, '0');
 
 const hcal = `https://www.hebcal.com/converter?cfg=json&gy=${yy}&gm=${mm}&gd=${dd}&g2h=1`;
 
-const caljson = fetchIt( hcal );
+const caljson = await fetchIt( hcal );
 
-console.log( `debug: caljson=="${JSON.stringify(caljson)}`);
+console.log( `debug: caljson=="${JSON.stringify(caljson)}"`);
 document.writeln( `Today's Hebrew date: ${caljson.hd} ${caljson.hm} ${caljson.hy} (${caljson.hebrew})`);
 console.log('script complete /escott')
