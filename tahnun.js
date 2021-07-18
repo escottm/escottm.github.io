5066f158f87414987c23f051c12ad7b825c26454
E. Scott Menter <scott.menter@gmail.com>
2021-07-17 21:19:41 -0700

    tahnun

---
+++ tahnun.js
@@ -0,0 +1,22 @@
+
+function tahnun_today() {
+    const hdate = 
+    {
+        day: document.getElementById('heb__date').innerHTML,
+        month: document.getElementById('heb__month').innerHTML
+    }
+
+    switch (hdate) {
+        case {day: '8', month: 'Av'}:
+        case {day: '9', month: 'Av'}:
+            return false;
+        default:
+            return true;
+    }
+}
+
+document.getElementById('tahnum').innerHTML = 
+    tahnun_today()
+    ? 'We recite ta&#7717nun today'
+    : 'NO TA&#7716NUN TODAY';
+
