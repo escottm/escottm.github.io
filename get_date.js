a78a9b0c33c64157f8c27abb916cab463a925fb3
E. Scott Menter <scott.menter@gmail.com>
2021-07-17 17:23:18 -0700

    You can't accuse me of being afraid to commit

---
+++ get_date.js
@@ -0,0 +1,18 @@
+
+export async function fetchIt( str ) {
+    const res = await fetch( str );
+    const ret = await res.json();
+    return ret;
+}
+
+const gregDate = new Date();
+const dd = gregDate.getDay().toString().padStart(2, '0');
+const mm = gregDate.getMonth().toString().padStart(2, '0');
+const yy = gregDate.getFullYear().toString().padStart(2, '0');
+
+const hcal = `https://www.hebcal.com/converter?cfg=json&gy=${yy}&gm=${mm}&gd=${dd}&g2h=1`;
+
+const caljson = fetchIt( hcal );
+
+document.writeln( `Today's Hebrew date: ${caljson.hd} ${caljson.hm} ${caljson.hy} (${caljson.hebrew})`);
+console.log('script complete /escott')
