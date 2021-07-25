3b724d0d621e0d52dbaad1e133f841e7162a5bf6
E. Scott Menter <scott.menter@gmail.com>
2021-07-25 13:17:07 -0700

    added mincha tachanun logic

--- ocegal.js
+++ ocegal.js
@@ -7,10 +7,19 @@ const tahanun = fetch( API )
     .then( json => distTahanunInfo(json))
     .catch( err => console.error(`ocegal: fetch() failed with "${err}"`));
 
+const found = ( ar, text ) => ( (ar !== undefined) && (ar.findIndex( s => s === text ) != -1));
+
 function distTahanunInfo( t ) {
     console.log(t);
     try {
-        document.getElementById('full__date').textContent =`${t.hm}-${t.hd}-${t.hy}`;
+        document.getElementById('full__date').textContent =`${t.hd} ${t.hm} ${t.hy}`;
+        const minha = found( t.services, 'minha');
+        const reciteAtMinha = (t.tahanun && minha) || ( !t.tahanun && !minha );
+        const tElemID = minha ? 'tahanun' : 'no__tahanun';
+        const tElemTxt = minha  
+                        ? "We recite ta&#7717anun today as usual"
+                        : "We do not recite ta&#7717anun this afternoon";
+        document.getElementById( tElemID ).textContent = tElemTxt;
     }
     catch (err) {
         console.error(`ocegal: unexpected error: "${err}"`)
