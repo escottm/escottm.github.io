9986c7746419efc25527597d67dc736060cd968b
E. Scott Menter <scott.menter@gmail.com>
2021-07-25 12:07:08 -0700

    hello world version

---
+++ ocegal.js
@@ -0,0 +1,16 @@
+
+const d = new Date();
+const API = `http://localhost:8080?date=${d.getMonth()+1}-${d.getDate()}-${d.getFullYear()}`;
+
+const tahanun = fetch( API )
+    .then( t => distTahanunInfo(t))
+    .catch( err => console.error(`ocegal: fetch() failed with "${err}"`));
+
+function distTahanunInfo( t ) {
+    try {
+        document.getElementById('full__date').textContent =`${t.hm}-${t.hd}-${t.hy}`;
+    }
+    catch (err) {
+        console.error(`ocegal: unexpected error: "${err}"`)
+    };
+}
