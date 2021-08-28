e2b9ad3c73161ecbb2b99c850d010ed49b71ecf4
E. Scott Menter <scott.menter@gmail.com>
2021-08-28 16:12:31 -0700

    dayjs.extend

--- ocegal.js
+++ ocegal.js
@@ -2,7 +2,7 @@ import * as dayjs from "./dayjs-1.10.6/dayjs.min.js";
 
 import * as customParseFormat from './dayjs-1.10.6/plugin/customParseFormat.js';
 
-extend(window.dayjs_plugin_customParseFormat);
+dayjs.extend(window.dayjs_plugin_customParseFormat);
 const qDate = window.location.search.replace(/\??/, "");
 
 console.log( `ocegal: /?${qDate}`);
