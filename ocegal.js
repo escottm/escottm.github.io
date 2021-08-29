b9450deff98025e97c43d9e3028cb632cf08f233
E. Scott Menter <scott.menter@gmail.com>
2021-08-29 14:33:01 -0700

    looking for new ways to do this

--- ocegal.js
+++ ocegal.js
@@ -1,6 +1,6 @@
-import * as dayjs from "./dayjs-1.10.6/dayjs.min.js";
+// import * as dayjs from "./dayjs-1.10.6/dayjs.min.js";
 
-import * as customParseFormat from './dayjs-1.10.6/plugin/customParseFormat.js';
+// import * as customParseFormat from './dayjs-1.10.6/plugin/customParseFormat.js';
 
 // dayjs.extend(customParseFormat);
 const qDate = window.location.search.replace(/\??/, "");
